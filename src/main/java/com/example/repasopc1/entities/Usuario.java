ef887873f3f1540600b4942b3cf9e75a1f19b6c1
LetiziaTorres <letizia.torres@utec.edu.pe>
2025-04-29 20:52:03 -0500

    feat: estructura base y entidades creadas

--- src/main/java/com/example/repasopc1/entities/Usuario.java
+++ src/main/java/com/example/repasopc1/entities/Usuario.java
@@ -1,4 +1,33 @@
 package com.example.repasopc1.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String password;
+
+
+
+
+
+
 }
